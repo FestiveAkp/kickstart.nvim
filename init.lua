5d87af37862b5f244b8592a259c847d2c417c131
Akash Pillai <akash.pillai@sproutsocial.com>
2025-01-07 11:29:05 -0600

    Fix config for which-key v3

--- init.lua
+++ init.lua
@@ -308,22 +308,18 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        -- ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-        ['<leader>ho'] = { name = 'Git [H]unk [O]pen', _ = 'which_key_ignore' },
-        ['b'] = { name = '[B]uffer', _ = 'which_key_ignore' },
-        ['bm'] = { name = '[B]uffer [M]ove', _ = 'which_key_ignore' },
+      require('which-key').add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        -- { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>ho', group = 'Git [H]unk [O]pen' },
+        { 'b', group = '[B]uffer' },
+        { 'bm', group = '[B]uffer [Move]' },
       }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
     end,
   },
 
